15b4d2e5c3252b7b8100e7fd4f2396bc898b7bb4
DarthPingu <blake_guyan@hotmail.com>
2020-03-11 03:53:53 +0000

    working with the api

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React, { useEffect, useState }from 'react'
+require('./Table.scss')
 function Table({props}) {
+  console.log(props, "props ")
+  function row(pilot,rowNum) {
+    return (
+      <div key={pilot.ffgpilot} className={`${rowNum} row`}>
+        <p>{pilot.prettyfaction}</p>
+        <p>{pilot.prettypilot}</p>
+      </div>
+    )
 
-  console.log('props', props)
+  }
+
   return(
-    <div>
-      <h2>TABLE</h2>
+    <div className="table">
+    {console.log(props, 'testing')}
+    {props.pilots.data.map((pilot, i) => {
+      let evenOdd = () =>{ if ( i % 2 == 0) {
+      	return 'even'
+      }else{
+      	return 'odd'
+      }}
+      return row(pilot, evenOdd())
+    })}
+      <h2>test</h2>
     </div>
   )
 }
--- src/containers/Pilots.js
+++ src/containers/Pilots.js
@@ -1,27 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {Table} from '../components'
+
 function Pilots() {
-  const [pilot, setPilot] = useState(0);
-
-  // Similar to componentDidMount and componentDidUpdate:
-
+  const [pilots, setPilots] = useState();
   useEffect(() => {
-    axios.get('https://www.pinksquadron.dk/pbm/api/pilotoverview.php?sid=59&fbclid=IwAR0PlfjtQzEsCVtQcP3Sy8Vs_JYz7jO-ocU6XS6UAfpcxrSb-rz_22DtjdA')
-    .then(res => {
-      const pilots = res.data
-      this.setPilot({pilots})
-      console.log(res.data)
-    })
-    // Update the document title using the browser API
+    if(!pilots) {
+      axios.get('https://www.pinksquadron.dk/pbm/api2/pilotoverview.php?sid=59&fbclid=IwAR3POP20jN5r8ZeITWC4_s44klV6Bt372-0B6nCTOZs8Z5yBTwUS1Gkb3Kg')
+      .then(res => {
+        setPilots(res);
+      })
+    }
+    if(pilots) {
+      let factions = {list:[]}
+      for(var i in pilots.data){
+        console.log(pilots.data[i].faction)
+        if(!factions.list || !factions.list.includes(pilots.data[i].faction)) {
+          factions.list.push(pilots.data[i].faction)
+          
+        }
 
-
-  });
+      }
+      console.log(factions,'factions')
+    }
+  })
+  // Similar to componentDidMount and componentDidUpdate:
+  let data = require('../data/data.json')
 
   return (
     <div>
       <h2>YO</h2>
-      <Table props={{key:"hello", value:'goodbye'}}/>
+      {pilots && <Table props={{pilots}}/>}
     </div>
   )
 }
